91a32de9bcf58cfcfbda74cf4477881615946a4c
Eun <eun@su.am>
2013-01-29 08:44:11 +0100

    first commit, ready to go

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using CefSharp.WinForms;
+using CefSharp;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        WebView webView;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webView = new WebView("http://www.google.com", new BrowserSettings());
+            webView.Dock = DockStyle.Fill;
+            this.Controls.Add(webView);
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# CefSharp - Embedded Chromium for .Net
+
+This is an example showing the use of CefSharp Winforms in C#.
+
+Use this as Template for your Quick'n'Dirty Webbrowser Projects.
+
+For more info see:
+https://github.com/chillitom/CefSharp
