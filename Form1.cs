60b522b1ba75d6a14c5e5db6af4f9044376b6e62
Eun <eun@su.am>
2013-01-29 09:14:36 +0100

    Update Form1.cs
    added PropertyChanged

--- Form1.cs
+++ Form1.cs
@@ -21,9 +21,35 @@ namespace Test
         private void Form1_Load(object sender, EventArgs e)
         {
             webView = new WebView("http://www.google.com", new BrowserSettings());
+            webView.PropertyChanged += new PropertyChangedEventHandler(webView_PropertyChanged);
             webView.Dock = DockStyle.Fill;
             this.Controls.Add(webView);
-            
         }
+        
+        // Get all propertys from https://github.com/chillitom/CefSharp/blob/master/CefSharp/BrowserCore.h
+        void webView_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            webView = (WebView)sender;
+            switch (e.PropertyName)
+            {
+                case "IsBrowserInitialized":
+                    //webView.IsBrowserInitialized
+                    break;
+                case "Title":
+                    //webView.Title
+                    break;
+                case "Address":
+                    //webView.Address
+                    break;
+                case "CanGoBack":
+                    //webView.CanGoBack
+                    break;
+                case "CanGoForward":
+                    //webView.CanGoForward;
+                    break;
+                case "IsLoading":
+                    //webView.IsLoading
+                    break;
+            }
     }
 }
